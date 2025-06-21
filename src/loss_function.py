8abdae997efab7c27281d3d2ead44fd2f825f601
nty-mac <1355705558@qq..com>
2025-06-22 00:39:31 +0800

    test: add tests for mean_square_error

--- src/loss_function.py
+++ src/loss_function.py
@@ -16,12 +16,12 @@ def mean_square_error(y_predict, y_real):
     :param y_real: real label value, a scalar or a numpy array
     :return: MSE loss
     """
-    return 0.5 * np.mean((y_predict ** 2 - y_real ** 2))
+    return np.mean((y_predict ** 2 - y_real ** 2))
 
 
 class Unittest(unittest.TestCase):
     def test_mean_square_error(self):
-        assert mean_square_error(np.array([1, 1, 1]), np.array([0, 0, 0])), 1
+        self.assertEquals(mean_square_error(np.array([1, 1, 1]), np.array([0, 0, 0])), 1)
 
 
 if __name__ == "__main__":
