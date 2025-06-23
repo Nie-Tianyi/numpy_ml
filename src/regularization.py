3118cd317891907ccc24bb7f668bacea5f6d1934
nty-mac <1355705558@qq..com>
2025-06-23 15:37:25 +0800

    docs: add comments for regularization.py & standardizer.py

--- src/regularization.py
+++ src/regularization.py
@@ -18,9 +18,23 @@ class RegularizationTerm(Enum):
 
 @numba.njit
 def ridge(weights, rg_param, m):
+    """
+    L2 regularization term
+    :param weights: model weights
+    :param rg_param: hyperparameter
+    :param m: number of data
+    :return: loss brought by regularization
+    """
     return (rg_param / 2 * m) * np.sum(weights ** 2)
 
 
 @numba.njit
 def ridge_gradient(weights, rg_param, m):
+    """
+    L2 regularization gradient term
+    :param weights: model weights
+    :param rg_param: hyperparameter
+    :param m: number of data
+    :return: gradient brought by regularization
+    """
     return (rg_param / m) * weights
--- src/standardizer.py
+++ src/standardizer.py
@@ -1,18 +1,36 @@
+"""
+重新缩放训练数据
+"""
+
 import unittest
 
 import numpy as np
 
 
 class Standardizer:
+    """
+    Z-score Normalisation scaler
+    """
+
     def __init__(self, mean, std):
         self.mean = mean
         self.std = std
 
     def rescale(self, x):
+        """
+        使用Z-score Normalisation 重新缩放数据
+        :param x: 需要缩放的数据
+        :return: 缩放后的数据
+        """
         return (x - self.mean) / self.std
 
 
 def standardization(x):
+    """
+    使用z-score Normalisation 重新缩放数据，返回缩放后的数据
+    :param x: 需要缩放的数据
+    :return: 返回缩放后的数据，以及一个缩放器，用于缩放验证数据
+    """
     scaler = Standardizer(np.mean(x, axis=0), np.std(x, axis=0))
     return scaler.rescale(x), scaler
 
