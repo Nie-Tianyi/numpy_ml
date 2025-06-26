bdff27371383c978e0e557e3e7ad513ae54c248d
nty-pc <1355705558@qq.com>
2025-06-26 17:10:41 +0800

    refactor: change linear model init function param name

---
+++ src/logistic_regression.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class LogisticRegressionModel:
+    def __init__(self):
+        pass
+
+
+class Unittest(unittest.TestCase):
+    def test_logistic_regression(self):
+        self.assertEqual(1 + 1, 2)
