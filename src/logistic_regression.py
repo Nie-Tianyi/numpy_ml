e8791dd041be01e728f84c92b86956cfe5e7a675
nty-pc <1355705558@qq.com>
2025-06-26 18:14:25 +0800

    feat: add logistic_regression.py

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,10 +1,19 @@
 import unittest
 
+import numpy as np
+
 
 class LogisticRegressionModel:
-    def __init__(self):
-        pass
+    def __init__(self, niter: int = 1000, learning_rate: float = 0.01, reg_param: float = 0.3):
+        self.weights = None
+        self.bias = None
+        self.niter = niter
+        self.lr = learning_rate
+        self.lambda_ = reg_param
+        self.loss_history = []
 
+    def fit(self, x: np.ndarray, y: np.ndarray):
+        pass
 
 class Unittest(unittest.TestCase):
     def test_logistic_regression(self):
