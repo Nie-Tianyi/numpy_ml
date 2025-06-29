d7238bfb7657bcc9c936168e32ec92de7e326706
nty-mac <1355705558@qq..com>
2025-06-29 18:16:33 +0800

    feat: add fit method for logistic regression model

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -2,18 +2,40 @@ import unittest
 
 import numpy as np
 
+from src.regularization import RegularizationTerm
+
 
 class LogisticRegressionModel:
-    def __init__(self, niter: int = 1000, learning_rate: float = 0.01, reg_param: float = 0.3):
+    """
+    Logistic Regression Model
+    """
+
+    def __init__(self, niter: int = 1000, learning_rate: float = 0.01, reg_param: float = 0.3,
+                 regularization=RegularizationTerm.RIDGE):
         self.weights = None
         self.bias = None
         self.niter = niter
         self.lr = learning_rate
         self.lambda_ = reg_param
+        self.regularization = regularization
         self.loss_history = []
 
     def fit(self, x: np.ndarray, y: np.ndarray):
-        pass
+        """
+        训练模型
+        :param x: 假设是一个 (m,n) shape的 numpy.ndarray，m表示有多少数据，n表示数据的维度
+        :param y: labels，应该是一个 (m,1) shape的 numpy.ndarray
+        """
+        (m, n) = x.shape
+
+        assert y.shape[0] == m, "x & y should have same length"
+
+        self.__init_weights_and_bias(n)
+
+    def __init_weights_and_bias(self, dim: int):
+        self.weights = np.random.randn(dim)
+        self.bias = np.zeros(1)
+
 
 class Unittest(unittest.TestCase):
     def test_logistic_regression(self):
