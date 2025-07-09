d31fb1b85763c0570ac0500f03adb8c88c62b22c
nty-pc <1355705558@qq.com>
2025-07-10 02:20:55 +0800

    feat: add test binary data generator

--- test_data_set/test_data_gen.py
+++ test_data_set/test_data_gen.py
@@ -23,3 +23,18 @@ def linear_data(data_size: int = 1000, seed=1):
     # 创建目标值 (1000,)
     y = 0.99 * x_1 + 2.3 * x_2 + noise + 1
     return x, y
+
+
+def binary_data(data_size=1000, seed=1):
+    """
+    """
+    np.random.seed(seed)
+
+    x_1 = np.random.rand(data_size)
+    x_2 = np.random.rand(data_size)
+    noise = np.random.rand(data_size)
+
+    x = np.stack([x_1, x_2], axis=1)
+    y = np.where(x_1 + x_2 + noise <= 1.5, 0, 1)
+
+    return x, y
