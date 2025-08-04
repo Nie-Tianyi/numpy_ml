db7c9270998c485c543e6f17d989034d04fd5171
nty-pc <1355705558@qq.com>
2025-08-05 00:14:47 +0800

    feat: add polynomial_logistic_regression.py

--- algorithms/polynomial_logistic_regression.py
+++ algorithms/polynomial_logistic_regression.py
@@ -1,20 +1,36 @@
 import unittest
-import numpy as np
-from numpy.typing import NDArray
+from typing import Optional
 
-from algorithms.regularization import Regularization
+import numba
+import numpy as np
+import seaborn
+from matplotlib import pyplot as plt
+
+from numpy.typing import NDArray
+from tqdm import tqdm
+
+from algorithms.activation_functions import softmax
+from algorithms.loss_function import sparse_cross_entropy_loss
+from test_data_set.mnist import mnist
+from test_data_set.test_data_gen import binary_data
+from algorithms.regularization import Regularization, lasso, ridge
 
 
 class PolynomialLogisticRegression:
+    weights: Optional[NDArray[np.float64]]
+    bias: Optional[NDArray[np.float64]]
+    labels: Optional[NDArray[np.float64]]
+
     def __init__(
         self,
         niter=1000,
-        learning_rate=0.01,
-        reg_param=0.3,
+        learning_rate= 1,
+        reg_param=0.03,
         regularization=Regularization.RIDGE,
     ):
         self.weights = None
         self.bias = None
+        self.labels = None
         self.niter = niter
         self.lr = learning_rate
         self.lambda_ = reg_param
@@ -26,16 +42,129 @@ class PolynomialLogisticRegression:
         self.weights = np.random.rand(dim, k)
         self.bias = np.zeros(k)
 
-    def fit(self, x, y):
-        pass
+    def fit(self, x: NDArray[np.float64], y: NDArray[np.float64]):
+        self.labels = np.unique(y)
+        print("possible labels:", self.labels)
+        # 把 y 从标签转换成 one-hot 编码 e.g. 9 => [0,0,0,0,0,0,0,0,0,1]
+        y = (y.reshape(-1, 1) == self.labels).astype(np.float64)
+
+        k = len(self.labels)
+        (m, n) = x.shape
+
+        self.__init_weights_and_bias(n, k)
+        for _ in tqdm(range(self.niter)):
+            y_pred = self.predict(x)
+
+            # 计算梯度
+            if self.reg == Regularization.No_REGULARIZATION:
+                loss = sparse_cross_entropy_loss(y_pred, y)  # 计算损失
+                self.loss_history.append(loss)  # 记录损失
+
+                (dlt_w, dlt_b) = self.__compute_gradient_without_regularization(
+                    x, y_pred, y
+                )
+                self.weights -= self.lr * dlt_w
+                self.bias -= self.lr * dlt_b
+            elif self.reg == Regularization.LASSO:
+                loss = sparse_cross_entropy_loss(y_pred, y) + lasso(
+                    self.weights, self.lambda_, m
+                )
+                self.loss_history.append(loss)
+
+                (dlt_w, dlt_b) = self.__compute_gradient_with_l1_regularization(
+                    x, y_pred, y, self.weights, self.lambda_
+                )
+                self.weights -= self.lr * dlt_w
+                self.bias -= self.lr * dlt_b
+            elif self.reg == Regularization.RIDGE:
+                loss = sparse_cross_entropy_loss(y_pred, y) + ridge(
+                    self.weights, self.lambda_, m
+                )
+                self.loss_history.append(loss)
+
+                (dlt_w, dlt_b) = self.__compute_gradient_with_l2_regularization(
+                    x, y_pred, y, self.weights, self.lambda_
+                )
+                self.weights -= self.lr * dlt_w
+                self.bias -= self.lr * dlt_b
+
+    @staticmethod
+    def __compute_gradient_without_regularization(x, y_pred, y_real):
+        m = x.shape[0]  # x.shape = (m, n)
+
+        err = y_pred - y_real  # err.shape = (m, K)
+        dlt_w = np.dot(x.T, err) / m
+        dlt_b = np.sum(err, axis=0) / m
+
+        return dlt_w, dlt_b
+
+    @staticmethod
+    def __compute_gradient_with_l2_regularization(x, y_pred, y_real, weights, lambda_):
+        m = x.shape[0]  # x.shape = (m, n)
+
+        err = y_pred - y_real  # err.shape = (m, K)
+        dlt_w = np.dot(x.T, err) / m
+        dlt_b = np.sum(err, axis=0) / m
+
+        dlt_w += (lambda_ / m) * weights  # l2 gradient
+
+        return dlt_w, dlt_b
+
+    @staticmethod
+    def __compute_gradient_with_l1_regularization(x, y_pred, y_real, weights, lambda_):
+        m = x.shape[0]  # x.shape = (m, n)
+
+        err = y_pred - y_real  # err.shape = (m, K)
+        dlt_w = np.dot(x.T, err) / m
+        dlt_b = np.sum(err, axis=0) / m
+
+        dlt_w += (lambda_ / m) * np.sign(weights)  # l1 gradient
+
+        return dlt_w, dlt_b
 
     def predict(self, x):
-        pass
+        if self.weights is None or self.bias is None:
+            raise ValueError("Model has not been initialised yet")
+        # x.shape = (m, n) self.weights.shape = (n, K)
+        z = np.dot(x, self.weights) + self.bias
+        return softmax(z, axis=1)
+
+    def plot_loss_history(self) -> None:
+        """
+        plot loss history
+        """
+        seaborn.lineplot(self.loss_history)
+        plt.title("Training Loss History")
+        plt.xlabel("Iteration")
+        plt.ylabel("Sparse Cross-Entropy Loss")
+        plt.show()
 
 
 class Unittest(unittest.TestCase):
-    def test_softmax(self):
-        pass
+    def test_polynomial_logistic_regression(self):
+        (x, y) = binary_data(
+            data_size=10000, seed=7
+        )  # 多分类问题当然也能处理而分类问题啦
+        model = PolynomialLogisticRegression(niter=1000, learning_rate=1, reg_param=0.01)
+        model.fit(x, y)
+
+        test_point = np.array([[1, 1]])
+        print(model.predict(test_point))
+        model.plot_loss_history()
+
+    def test_mnist(self):
+        (x, y) = mnist(data_size=100, seed=7)
+
+        def reshape_x(arr):
+            # 将 n*28*28 的 image 拍平成 n*784 的二维数组
+            return arr.reshape(arr.shape[0], -1)
+
+        x = reshape_x(x)
+        model = PolynomialLogisticRegression(niter=1000, learning_rate=1, reg_param=0.01)
+        model.fit(x, y)
+        model.plot_loss_history()
+
+        self.assertEqual(1 + 1, 2)
 
 
 if __name__ == "__main__":
