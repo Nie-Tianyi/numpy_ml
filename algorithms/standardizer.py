a32575b0b5fdd2a0de9351896112ae7d56dcaa7f
nty-pc <1355705558@qq.com>
2025-08-05 13:42:28 +0800

    fix: set a special case for standardizer.py when the std is 0

--- algorithms/standardizer.py
+++ algorithms/standardizer.py
@@ -15,6 +15,7 @@ class Standardizer:
     def __init__(self, mean, std):
         self.mean = mean
         self.std = std
+        self.std[std == 0] = 1
 
     def rescale(self, x):
         """
@@ -28,19 +29,30 @@ class Standardizer:
 def standardization(x):
     """
     使用z-score Normalisation 重新缩放数据，返回缩放后的数据
+    如果数据的标准差为0，则不做任何缩放
     :param x: 需要缩放的数据
     :return: 返回缩放后的数据，以及一个缩放器，用于缩放验证数据
     """
-    scaler = Standardizer(np.mean(x), np.std(x))
+    scaler = Standardizer(np.mean(x, axis=0), np.std(x, axis=0))
     return scaler.rescale(x), scaler
 
 
 class Unittest(unittest.TestCase):
     def test_standardizer(self):
         arr = np.arange(0, 10).reshape((5, 2))
+        print("before normalise:", arr)
         rescaled_arr, scaler = standardization(arr)
+        print("after normalise:", rescaled_arr)
         self.assertEqual(arr.shape, rescaled_arr.shape)
 
+    def test_standardizer_with_zeros(self):
+        arr = np.zeros((5, 2), dtype=np.float64)
+        print("before normalise:", arr)
+        rescaled_arr, scaler = standardization(arr)
+        print("mean, std", scaler.mean, scaler.std)
+        print("after normalise:", rescaled_arr)
+        self.assertEqual(arr.shape, rescaled_arr.shape)
+        np.testing.assert_almost_equal(rescaled_arr, arr)
 
 if __name__ == "__main__":
     unittest.main()
