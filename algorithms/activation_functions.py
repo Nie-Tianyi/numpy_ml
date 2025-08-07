b4dbb830404947c15ff06e09785f94275b3c932d
nty-pc <1355705558@qq.com>
2025-08-07 20:07:30 +0800

    feat: add ReLU

--- algorithms/activation_functions.py
+++ algorithms/activation_functions.py
@@ -19,10 +19,21 @@ def sigmoid(x):
 	return 1 / (1 + np.exp(-x))
 
 
+# numba不支持axis属性，所以这个函数不能使numba加速
 def softmax(x: NDArray[np.float64], axis=0):
 	x = x - np.max(x, axis=axis, keepdims=True)  # 平移防止溢出
 	ex = np.exp(x)
 	return ex / ex.sum(axis=axis, keepdims=True)
+
+
+@numba.njit(fastmath=True)
+def relu(x):
+	"""
+	ReLU Rectified Linear Unit 激活函数
+	:param x: x
+	:return: ReLU(x)
+	"""
+	return max(0, x)
 
 
 class Unittest(unittest.TestCase):
