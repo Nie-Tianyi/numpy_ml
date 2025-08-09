ff517db6af7a73289cc3a1a6cfd15a3f94dd1b53
nty-mac <1355705558@qq..com>
2025-08-10 02:17:45 +0800

    docs: amend comments

--- algorithms/nerual_network.py
+++ algorithms/nerual_network.py
@@ -8,6 +8,9 @@ from algorithms.regularization import Regularization
 
 
 class NeuralNetwork(MachineLearningModel):
+	"""
+	神经网络
+	"""
 	def __init__(
 		self,
 		layers,
@@ -30,6 +33,9 @@ class NeuralNetwork(MachineLearningModel):
 
 
 class LinearLayer:
+	"""
+	线性层，默认使用ReLU作为激活函数
+	"""
 	def __init__(self, num, activation_function=relu):
 		self.num = num  # 一个线性层
 		self.activation_function = activation_function  # 激活函数，默认是ReLU
