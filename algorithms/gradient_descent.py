155c2af6619575201dd26c74ddbd2616f57ca032
nty-pc <1355705558@qq.com>
2025-08-11 17:18:23 +0800

    docs: add comments

--- algorithms/gradient_descent.py
+++ algorithms/gradient_descent.py
@@ -7,7 +7,7 @@ def compute_gradient(
 ):
 	"""
 	计算梯度
-	:param x: 输入数据，shape为(m,n)
+	:param x: 输入数据，shape为(m,n)，m为样本数量，n为特征数量
 	:param y_pred: 模型预测值，shape为(m,1)，多分类问题则为(m,K)
 	:param y_real: 真实值，shape为(m,1)，多分类问题则为(m,K)
 	:return: 梯度，dlt_w的shape为(n,1), dlt_b的shape为(1,1)；多分类问题则为(n,K)
