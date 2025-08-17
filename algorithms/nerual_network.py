8863e328b03bac21d604b34db585c5b527916106
nty-pc <1355705558@qq.com>
2025-08-18 00:16:55 +0800

    refactor: move NeuralNetworkLayer to a separate file

--- algorithms/nerual_network.py
+++ algorithms/nerual_network.py
@@ -1,85 +1,12 @@
 import unittest
-from abc import ABC, abstractmethod
 from typing import List
 
-import numpy as np
+from tqdm import tqdm
 
-from algorithms.activation_functions import ReLU, ActivationFunction
+from algorithms.activation_functions import Sigmoid
 from algorithms.model_abstract import MachineLearningModel
+from algorithms.neural_network_layer import NeuralNetworkLayer, LinearLayer
 from algorithms.regularization import Regularization, Ridge
-
-
-class NeuralNetworkLayer(ABC):
-	@abstractmethod
-	def forward(self, x):
-		pass
-
-	@abstractmethod
-	def backward(self, error):
-		pass
-
-
-class LinearLayer(NeuralNetworkLayer):
-	"""
-	线性层，默认使用ReLU作为激活函数
-	"""
-
-	def __init__(
-		self,
-		num,
-		activation_function: ActivationFunction = ReLU,
-		reg: type[Regularization] = Ridge,
-		reg_params=0.1,
-	):
-		self.num = num  # 神经元数量
-		self.activation_function = activation_function  # 激活函数，默认是ReLU
-		self.reg = reg  # 正则化，默认是L2正则
-		self.lambda_ = reg_params  # 正则化超参数，默认0.1
-		self.weights = None  # 权重，形状为 (num, dim)
-		self.bias = None  # 偏置，形状为 (num,)
-		self.z = None  # 线性输出，形状为 (m, num)，m是输入数据的长度
-		self.inputs = None  # 上一层的激活输出，这一层的输入，形状为 (m, num)
-
-	def _init_weights_and_bias(self, dim):
-		self.weights = np.random.randn(self.num, dim)
-		self.bias = np.zeros(self.num)
-
-	def forward(self, x):
-		"""
-		前向传播
-		:param x: 输入，形状为 (m, dim)
-		:return: 输出，形状为 (m, num)
-		"""
-		self.inputs = x
-		# weights.shape = (num, dim) x.shape = (m, dim)
-		self.z = np.dot(x, self.weights.T) + self.bias
-		# z.shape = (m, num)
-		a = self.activation_function.cal(self.z)
-		return a
-
-	def backward(self, error):
-		"""
-		反向传播：更新自己的权重，然后返回下一层的误差 delta
-		:param error: 这一层的误差，**不包括这一层激活函数的梯度**，形状为 (m, num)
-		:return: 下一层的误差，**同样也不包括下一层的激活函数的梯度**，形状为 (m, dim)
-		"""
-		m = error.shape[0]
-		# 先把激活函数的梯度算上
-		error = error * self.activation_function.derivative(self.z)  # error.shape = (m, num)
-
-		# 计算下一层的error（要在更新参数之前）
-		prev_layer_error = np.dot(error, self.weights)
-
-		# 计算梯度 更新参数
-		dlt_w = (1 / m) * np.dot(error.T, self.inputs)  # dlt_w.shape = (num, dim)
-		dlt_b = (1 / m) * np.sum(error)  # dlt_b.shape = (num,)
-		# 加上正则化带来的梯度
-		dlt_w += self.reg.derivative(self.weights, self.lambda_, m)
-		# 更新参数
-		self.weights -= dlt_w
-		self.bias -= dlt_b
-
-		return prev_layer_error
 
 
 class NeuralNetwork(MachineLearningModel):
@@ -98,11 +25,25 @@ class NeuralNetwork(MachineLearningModel):
 		super().__init__(regularization, niter, learning_rate, reg_param)
 		self.layers = layers
 
+	def __init_weights_and_bias(self, dim):
+		for layer in self.layers:
+			layer.init_weights_and_bias(dim)
+			dim = layer.num
+
 	def fit(self, x, y):
+		# 先初始化神经网络的参数
+		(m, dim) = x.shape
+		self.__init_weights_and_bias(dim)
+
+		for _ in tqdm(range(self.niter)):
+			y_hat = self.predict(x)
+
 		pass
 
 	def predict(self, x):
-		pass
+		for layer in self.layers:
+			x = layer.forward(x)
+		return x
 
 	def evaluate(self, x_test, y_test) -> float:
 		pass
@@ -110,6 +51,10 @@ class NeuralNetwork(MachineLearningModel):
 
 class Unittest(unittest.TestCase):
 	def test_neural_network(self):
+		neural_network = NeuralNetwork(
+			[LinearLayer(4), LinearLayer(3), LinearLayer(1, activation_function=Sigmoid)]
+		)
+
 		self.assertEqual(1 + 1, 2)
 
 
---
+++ algorithms/neural_network_layer.py
@@ -0,0 +1,96 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from algorithms.activation_functions import ActivationFunction, ReLU
+from algorithms.regularization import Regularization, Ridge
+
+
+# 定义trait
+class NeuralNetworkLayer(ABC):
+	@property
+	@abstractmethod
+	def num(self):
+		"""获取这一层神经网络有多少个神经元"""
+		pass
+
+	@abstractmethod
+	def init_weights_and_bias(self, dim):
+		"""初始化这一层的参数"""
+		pass
+
+	@abstractmethod
+	def forward(self, x):
+		"""前向传播"""
+		pass
+
+	@abstractmethod
+	def backward(self, error):
+		"""反向传播，跟新参数"""
+		pass
+
+
+class LinearLayer(NeuralNetworkLayer):
+	"""
+	全连接线性层，默认使用ReLU作为激活函数
+	"""
+
+	def __init__(
+		self,
+		num,
+		activation_function: type[ActivationFunction] = ReLU,
+		reg: type[Regularization] = Ridge,
+		reg_params=0.1,
+	):
+		self.num = num  # 神经元数量
+		self.activation_function = activation_function  # 激活函数，默认是ReLU
+		self.reg = reg  # 正则化，默认是L2正则
+		self.lambda_ = reg_params  # 正则化超参数，默认0.1
+		self.weights = None  # 权重，形状为 (num, dim)
+		self.bias = None  # 偏置，形状为 (num,)
+		self.z = None  # 线性输出，形状为 (m, num)，m是输入数据的长度
+		self.inputs = None  # 上一层的激活输出，这一层的输入，形状为 (m, num)
+
+	def num(self):
+		return self.num
+
+	def init_weights_and_bias(self, dim):
+		self.weights = np.random.randn(self.num, dim)
+		self.bias = np.zeros(self.num)
+
+	def forward(self, x):
+		"""
+		前向传播
+		:param x: 输入，形状为 (m, dim)
+		:return: 输出，形状为 (m, num)
+		"""
+		self.inputs = x
+		# weights.shape = (num, dim) x.shape = (m, dim)
+		self.z = np.dot(x, self.weights.T) + self.bias
+		# z.shape = (m, num)
+		a = self.activation_function.cal(self.z)
+		return a
+
+	def backward(self, error):
+		"""
+		反向传播：更新自己的权重，然后返回下一层的误差 delta
+		:param error: 这一层的误差，**不包括这一层激活函数的梯度**，形状为 (m, num)
+		:return: 下一层的误差，**同样也不包括下一层的激活函数的梯度**，形状为 (m, dim)
+		"""
+		m = error.shape[0]
+		# 先把激活函数的梯度算上
+		error = error * self.activation_function.derivative(self.z)  # error.shape = (m, num)
+
+		# 计算下一层的error（要在更新参数之前）
+		prev_layer_error = np.dot(error, self.weights)
+
+		# 计算梯度 更新参数
+		dlt_w = (1 / m) * np.dot(error.T, self.inputs)  # dlt_w.shape = (num, dim)
+		dlt_b = (1 / m) * np.sum(error)  # dlt_b.shape = (num,)
+		# 加上正则化带来的梯度
+		dlt_w += self.reg.derivative(self.weights, self.lambda_, m)
+		# 更新参数
+		self.weights -= dlt_w
+		self.bias -= dlt_b
+
+		return prev_layer_error
