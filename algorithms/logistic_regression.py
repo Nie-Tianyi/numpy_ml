b746d99a6c71696ff001a79f3c1807d88c0b910d
nty-pc <1355705558@qq.com>
2025-08-19 15:05:46 +0800

    test: add test for neural network, fix bug in evaluation method

--- algorithms/logistic_regression.py
+++ algorithms/logistic_regression.py
@@ -115,7 +115,7 @@ class Unittest(unittest.TestCase):
         model_no_scaled.fit(x, y)
 
         # 处于 x_1 + x_2 = 1 右边的点预测结果应该大于0.5，并且离决策边际越远，预测结果越接近于1
-        test_point = np.array([[1, 1]])
+        test_point = np.array([[0, 0]])
         res = model.predict(scalar.rescale(test_point))[0]
         print("\nFinal Results:")
         print(f"Predicted: {res:.4f}")  # 0.9967，有99.67%的概率这个点是1
--- algorithms/nerual_network.py
+++ algorithms/nerual_network.py
@@ -2,10 +2,12 @@ import unittest
 from abc import ABC
 from typing import List
 
+import numpy as np
 from overrides import overrides
 from tqdm import tqdm
 
 from algorithms.evaluation import Accuracy, EvaluationMethod
+from algorithms.logistic_regression import LogisticRegressionModel
 from algorithms.loss_function import cross_entropy_loss
 from algorithms.model_abstract import MachineLearningModel
 from algorithms.neural_network_layer import LinearLayer, NeuralNetworkLayer, SigmoidOutputLayer
@@ -46,7 +48,6 @@ class NeuralNetwork(MachineLearningModel, ABC):
         for _ in tqdm(range(self.niter)):
             y_hat = self.forward_propagation(x)
             reg_loss = self.backward_propagation(y_hat - y)
-            print(reg_loss)
             self.loss_history.append(self.loss_function(y_hat, y) + reg_loss)
 
     def predict(self, x):
@@ -70,19 +71,23 @@ class BinaryClassificationNeuralNetwork(NeuralNetwork):
         self, x_test, y_test, evaluation_method: type[EvaluationMethod] = Accuracy
     ) -> float:
         y_hat = self.predict(x_test)
-        y_hat = (y_hat >= self.threshold).astype(int)
+        y_hat = (y_hat >= self.threshold).astype(float).flatten()
         return evaluation_method.evaluate(y_hat, y_test)
 
     def __init__(
         self,
+        layers=None,
         threshold: float = 0.5,
         niter=1000,
         learning_rate=0.1,
         reg_param: float = 0.3,
         regularization: type[Regularization] = Ridge,
     ):
+        if layers is None:
+            layers = [LinearLayer(4), LinearLayer(5), SigmoidOutputLayer()]
+
         super().__init__(
-            [LinearLayer(4), LinearLayer(5), SigmoidOutputLayer()],
+            layers,
             cross_entropy_loss,
             niter,
             learning_rate,
@@ -95,18 +100,42 @@ class BinaryClassificationNeuralNetwork(NeuralNetwork):
 class Unittest(unittest.TestCase):
     def test_neural_network(self):
         neural_network = BinaryClassificationNeuralNetwork(
-            threshold=0.5, niter=1000, learning_rate=0.001
+            threshold=0.5, niter=2000, learning_rate=0.01
         )
+        logistic_model = LogisticRegressionModel(
+            threshold=0.5, niter=2000, learning_rate=0.01
+        )  # benchmark
 
         (x, y) = binary_data(data_size=10000, seed=78)
         rescaled_x, scaler = z_score_normalisation(x)
+
         neural_network.fit(rescaled_x, y)
-        neural_network.plot_loss_history(label="Cross Entropy Loss")
+        neural_network.plot_loss_history(title="Neural Network's Loss", label="Cross Entropy Loss")
+        logistic_model.fit(rescaled_x, y)
+        logistic_model.plot_loss_history(title="Logistic Model's Loss", label="Cross Entropy Loss")
 
         (test_x, test_y) = binary_data(data_size=1000, seed=79)
         rescaled_test_x = scaler.rescale(test_x)
-        acc = neural_network.evaluate(rescaled_test_x, test_y)
-        print("Model Accuracy: ", acc)
+        acc_nn = neural_network.evaluate(rescaled_test_x, test_y)
+        acc_logs = logistic_model.evaluate(rescaled_test_x, test_y)
+        print("Neural Network Model's Accuracy: ", acc_nn) # 神经网络的Accuracy反而比Logistic Model少一点，可能有点过拟合
+        print("Logistic Model's Accuracy:", acc_logs)
+        print("Neural Network Final Loss:", neural_network.loss_history[-1])
+        print("Logistic Model Final Loss:", logistic_model.loss_history[-1])
+        print("---------------------------------------------------")
+        print("Test Point (1,1) predicted result should be close to 1")
+        p1 = scaler.rescale(np.array([[1, 1]]))
+        res1_nn = neural_network.predict(p1)
+        print("Neural Network's Result:", res1_nn)
+        res1_logs = logistic_model.predict(p1)
+        print("Logistic Model's Result:", res1_logs)
+
+        print("Test Point (0,0) predicted result should be close to 0")
+        p2 = scaler.rescale(np.array([[0, 0]]))
+        res2_nn = neural_network.predict(p2)
+        print("Neural Network's Result:", res2_nn)
+        res2_logs = logistic_model.predict(p2)
+        print("Logistic Model's Result:", res2_logs)
 
         self.assertEqual(1 + 1, 2)
 
