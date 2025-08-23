a2d3f0b65e1f70b0aedbaa2b3f0ab84f8a5b4337
nty-pc <1355705558@qq.com>
2025-08-24 00:56:39 +0800

    feat: add metric for measure distance between vectors

---
+++ algorithms/distance_metrics.py
@@ -0,0 +1,70 @@
+"""
+衡量两个向量之间距离或者相似度的算法
+"""
+
+import unittest
+from abc import ABC, abstractmethod
+
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Metric(ABC):
+    @staticmethod
+    @abstractmethod
+    def distance(x, y):
+        """
+        x, y 应该是第二个维度相同的两个向量
+        """
+        pass
+
+
+class EuclidianDistance(Metric):
+    """
+    欧几里得距离
+    """
+
+    @staticmethod
+    def distance(x: NDArray[np.float64], y: NDArray[np.float64]):
+        assert x.shape[1] == y.shape[1]
+
+        return np.sqrt(np.sum((x - y) ** 2, axis=1))
+
+
+class CosineDistance(Metric):
+    """
+    余弦相似度
+    """
+
+    @staticmethod
+    def distance(x, y):
+        assert x.shape[1] == y.shape[1]
+
+        return 1 - CosineDistance.similarity(x, y)
+
+    @staticmethod
+    def similarity(x, y):
+        assert x.shape[1] == y.shape[1]
+
+        return np.sum(x * y, axis=1) / (
+            np.sqrt(np.sum(x**2, axis=1)) * np.sqrt(np.sum(y**2, axis=1))
+        )
+
+
+class Unittest(unittest.TestCase):
+    def test_euclidean_distance(self):
+        x = np.array([[1, 2], [1, 0], [2, 1], [1, 1]])
+        y = np.array([[1, 1]])
+
+        self.assertTrue(np.allclose(EuclidianDistance.distance(x, y), np.array([1, 1, 1, 0])))
+
+    def test_cosine_distance(self):
+        x = np.array([[1, 2], [1, 0], [2, 1], [1, 1]])
+        y = np.array([[1, 1]])
+
+        distance = CosineDistance.distance(x, y)
+        print(distance)
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == "__main__":
+    unittest.main()
