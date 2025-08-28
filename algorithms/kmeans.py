5b353ce18e5af9a95a957d84198e60bb5c778a10
nty-pc <1355705558@qq.com>
2025-08-28 23:55:15 +0800

    feat: add exploring function in kmeans.py

--- algorithms/kmeans.py
+++ algorithms/kmeans.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.typing import NDArray
+from tqdm import tqdm
 
 from algorithms.distance_metrics import EuclidianDistance, Metric
 from test_data_set.cluster_points import cluster_points_data
@@ -94,6 +95,41 @@ class KMeans:
         return self.loss_history[-1].loss
 
 
+def exploring_data_with_kmeans(
+    data,
+    k_range,
+    random_state_range: Optional[List[int]] = None,
+    distance_metric: type[Metric] = EuclidianDistance,
+    max_iter=1000,
+):
+    """
+    用 KMeans 算法探索数据，尝试不同的K值，画出损失随K值变化曲线
+    :param data: 需要被探索的数据
+    :param k_range: 尝试的k值
+    :param random_state_range: 每个K值会尝试多个random state以尝试找到全局最小值
+    :param distance_metric: 用什么算法衡量向量距离，默认使用 EuclideanDistance
+    :param max_iter: 每个KMeans算法最多循环多少次
+    :return:
+    """
+    if random_state_range is None:
+        random_state_range = range(10)
+
+    ultimate_final_losses = []
+    for k in tqdm(k_range):
+        final_losses = []
+        for random_state in random_state_range:
+            kmeans = KMeans(k, max_iter, distance_metric)
+            kmeans.fit(data, random_state)
+            final_losses.append(kmeans.final_loss)
+        ultimate_final_losses.append(min(final_losses))
+
+    plt.plot(k_range, ultimate_final_losses)
+    plt.title(label="Losses variation with different K")
+    plt.xlabel("K value")
+    plt.ylabel("Distortion Loss")
+    plt.show()
+
+
 class Unittest(unittest.TestCase):
     def test_kmeans(self):
         data, real_label = cluster_points_data(k=3, data_size=150, seed=135, min_distance=10)
@@ -107,6 +143,12 @@ class Unittest(unittest.TestCase):
         plt.scatter(data[:, 0], data[:, 1], c=predicted_label)
         plt.title(label="Predicted Cluster")
         plt.show()  # 很容易卡在Local Minimum啊
+
+        self.assertEqual(1 + 1, 2)
+
+    def test_explore(self):
+        data, real_label = cluster_points_data(k=6, data_size=150, seed=135, min_distance=10)
+        exploring_data_with_kmeans(data, k_range=range(1, 10))
 
         self.assertEqual(1 + 1, 2)
 
